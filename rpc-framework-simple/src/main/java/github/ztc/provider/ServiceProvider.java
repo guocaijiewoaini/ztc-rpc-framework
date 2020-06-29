6c82e5e393a1fb404f7dc8aa55765f2de1979a01
朱天成 <zhutiancheng@lizhi.fm>
2020-06-29 20:10:58 +0800

    开始写rpc

---
+++ rpc-framework-simple/src/main/java/github/ztc/provider/ServiceProvider.java
@@ -0,0 +1,16 @@
+package github.ztc.provider;
+/**
+ * @title: ServiceProvider
+ * @description: rpc提供服务接口
+ * @author: ztc
+ * @date: 2020/06/29 07:59 19:59
+ * @version: V1.0
+*/
+public interface ServiceProvider {
+
+    void addServiceProvider();
+
+
+    void getServiceProvider();
+    
+}
